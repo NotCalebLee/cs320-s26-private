8b2584318202875b98208248b73fb34c70c8fd0f
Caleb <calebjlee2006@gmail.com>
2026-03-02 22:42:55 +0000

    example deriv

--- assigns/assign4/lib/assign4.ml
+++ assigns/assign4/lib/assign4.ml
@@ -358,4 +358,29 @@ let interp (s : string) : (ty_deriv * value option, error) result  =
   )
   | None -> Error Parse_error
 
-let example_deriv : string = "" (* TODO *)
+let example_deriv : string = "((if (= (= 5 (+ 1 4)) (= 0 1)) (+ 2 3) (* (+ 4 5) 67)) int IF
+  ((= (= 5 (+ 1 4)) (= 0 1)) bool EQ
+    ((= 5 (+ 1 4)) bool EQ
+      (5 int INTLIT)
+      ((+ 1 4) int ADDINT
+        (1 int INTLIT)
+        (4 int INTLIT)
+      )
+    )
+    ((= 0 1) bool EQ
+      (0 int INTLIT)
+      (1 int INTLIT)
+    )
+  )
+  ((+ 2 3) int ADDINT
+    (2 int INTLIT)
+    (3 int INTLIT)
+  )
+  ((* (+ 4 5) 67) int MULINT
+    ((+ 4 5) int ADDINT
+      (4 int INTLIT)
+      (5 int INTLIT)
+    )
+    (67 int INTLIT)
+  )
+)"
